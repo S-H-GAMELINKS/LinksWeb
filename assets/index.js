5bc078e446636718e57ab469c4a62449352a7325
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-09-28 23:26:33 +0900

    fix index.js for using bootstrap-umi

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,8 @@
 import Vue from 'vue/dist/vue.esm';
+import * as  Bootstrap from 'bootstrap-umi';
+import 'bootstrap-umi/dist/css/bootstrap.css';
+
+Vue.use(Bootstrap);
 
 const links = new Vue({
     el: '#links',
