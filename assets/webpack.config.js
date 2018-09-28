52e68a52e38e4e4e76e1453a829e602c546eba2c
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-09-28 23:29:34 +0900

    fix webpack.config.js for using vue component

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,3 +1,5 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
     entry: './index.js', 
     output: {
@@ -6,6 +8,10 @@ module.exports = {
     },
     module: {
         rules: [
+        {
+            test: /\.vue$/,
+            use: 'vue-loader'
+        },
         {
             test: /\.css$/,
             use: [
@@ -19,4 +25,7 @@ module.exports = {
           'vue$': 'vue/dist/vue.esm.js'
         }
     },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
 };
