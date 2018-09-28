b3b902715577d6e9a87faa3e8cc59a2559583a27
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-09-28 23:48:05 +0900

    using vue-router for moved SPA

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue/dist/vue.esm';
+import Router from './router/router';
 import Header from './components/layouts/header.vue';
 import * as  Bootstrap from 'bootstrap-umi';
 import 'bootstrap-umi/dist/css/bootstrap.css';
@@ -6,6 +7,7 @@ import 'bootstrap-umi/dist/css/bootstrap.css';
 Vue.use(Bootstrap);
 
 const links = new Vue({
+    router: Router,
     el: '#links',
     components: {
         'nav-bar': Header
